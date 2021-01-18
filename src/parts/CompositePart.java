75897f0377564f519a908880138e19173dbca1ce
SZabara <SZabara@SZabara-NB.luxoft.com>
2021-01-18 23:01:51 +0200

    class structure was defined, as well as a complex composite for display was added

--- src/parts/CompositePart.java
+++ src/parts/CompositePart.java
@@ -1,5 +1,72 @@
 package parts;
 
-public class CompositePart {
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.handlers.IHandlerService;
+import org.eclipse.ui.part.ViewPart;
+
+import swt.elements.ComplexComposite;
+import ua.com.rcp.zabara.Utils;
+
+
+/**
+ * 
+ * View, which is responsible for displaying the user menu. This includes fields for editing data of the selected
+ * Person, and CRUD buttons.
+ * 
+ * @author SZabara
+ *
+ */
+public class CompositePart extends ViewPart {
+
+    public static final String ID = "ua.test.rcp.zabara.view.composite";
+
+    private ComplexComposite complexComposite;
+
+    public ComplexComposite getComplexComposite() {
+        return complexComposite;
+    }
+
+    @Override
+    public void createPartControl(Composite parent) {
+        complexComposite = new ComplexComposite(parent, SWT.NONE);
+        IHandlerService service = (IHandlerService) getSite().getService(IHandlerService.class);
+        complexComposite.getNewButton().addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                Utils.executeCommand(Utils.COMMAND_NEW, service);
+
+            }
+        });
+        complexComposite.getSaveButton().addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                Utils.executeCommand(Utils.COMMAND_SAVE, service);
+
+            }
+        });
+
+        complexComposite.getResetButton().addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                Utils.executeCommand(Utils.COMMAND_CANCEL, service);
+
+            }
+        });
+
+        complexComposite.getDeleteButton().addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent e) {
+                Utils.executeCommand(Utils.COMMAND_DELETE, service);
+
+            }
+        });
+
+    }
+
+    @Override
+    public void setFocus() {
+        complexComposite.setFocus();
+
+    }
 
 }
