4fb0a5f8a056ffca6e5cff61caa3f3f4f6de9f61
SZabara <SZabara@SZabara-NB.luxoft.com>
2021-01-18 14:49:44 +0200

    Structure of the simple rcp project was created

---
+++ src/ua/com/rcp/zabara/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package ua.com.rcp.zabara;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "ua.com.rcp.zabara.perspective"; //$NON-NLS-1$
+
+	@Override
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+    
+    @Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
---
+++ src/ua/com/rcp/zabara/Perspective.java
@@ -0,0 +1,11 @@
+package ua.com.rcp.zabara;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override	
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
