e1f4e9648132a0aaada57d62dceb9cc1022904ef
SZabara <SZabara@SZabara-NB.luxoft.com>
2021-02-01 13:00:08 +0200

    remove empty lines

--- src/ua/com/rcp/zabara/SelectionTester.java
+++ src/ua/com/rcp/zabara/SelectionTester.java
@@ -17,28 +17,20 @@ public class SelectionTester extends PropertyTester {
 
     @Override
     public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-
         if ("hasNonEmptyTextSelection".equals(property)) {
 
             IViewPart viewPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-
                     .findView(TableViewerPart.ID);
 
             TableViewerPart tableViewerPart = null;
-
             if (viewPart != null) {
-
                 try {
                     tableViewerPart = (TableViewerPart) viewPart;
-
                 } catch (ClassCastException exception) {
-
                     LOG.warn("Some problem with  classes cast. Current method can return incorrect result");
                 }
             }
-
             if (tableViewerPart.getCurrentPerson() != null) {
-
                 return true;
             }
         }
